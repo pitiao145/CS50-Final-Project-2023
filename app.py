325d1ca2273d3ade030ac50b553343fc94bed820
pitiao145 <pierre.bruyninckx1@gmail.com>
2023-06-29 20:02:43 +0800

    * Built a draft for the lay-out of the HTML pages     - layout.html     - login.html * Created a python file for the back-end * Incorporated flask and jinja * Basic styling with css

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from helpers import login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+#db = SQL("sqlite:///finance.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+   
+    return 0
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        ...
+    else:
+        return render_template("login.html")
