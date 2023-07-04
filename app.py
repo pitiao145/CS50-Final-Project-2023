9255fd758c13bc0f2dbf9419265cafd967752e1a
pitiao145 <pierre.bruyninckx1@gmail.com>
2023-07-04 14:44:45 +0800

    * Added contact page     -   Within contact page I added a form to contact the host     by email, using the flask_mail library *  Added reviews and mybeans pages and routes *  I  now have a basic skeleton for the site, containing all its     different routes and pages, with basic styling *  Next step will be to implement the server-side functionalities.

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from flask_mail import Mail, Message
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 from helpers import login_required
+from mailconfig import mail_username, mail_password
 
 # Configure application
 app = Flask(__name__)
+
+# Configue mail application
+app.config['MAIL_SERVER']='smtp-mail.outlook.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -29,11 +40,51 @@ def after_request(response):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        ...
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash('Must provide username')
+            return render_template("login.html")
+            
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash('Must provide password')
+            return render_template("login.html")
+  
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            flash('Invalid username and/or password')
+            return render_template("login.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        flash('Logged in!')
+        # Redirect user to home page
+        return redirect("/")
+
     else:
         return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -85,13 +136,43 @@ def register():
     else:
         return render_template("register.html")
 
-
 @app.route("/")
-##@login_required##
+@login_required
 def index():
     return render_template("index.html")
         
 @app.route("/brewing")
-##@login_required
+@login_required
 def brewing():
     return render_template("brewing.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+@login_required
+def contact():
+    if request.method == "POST":
+        fname = request.form.get("firstname")
+        lname = request.form.get("lastname")
+        email = request.form.get("email")
+        message = request.form.get("subject")
+
+        print(f"Name: {fname}\nLast name: {lname}\ne-mail: {email}\nMessage: {message}")
+
+        msg = Message(subject=f"Contact form message from {fname} {lname}:", sender = mail_username, recipients = ['pierre_bruyninckx@hotmail.com'])
+        msg.body = f"Name: {fname}\nLast name: {lname}\ne-mail: {email}\nMessage: {message}"
+        mail.send(msg)
+
+        flash('Form sent.')
+        return render_template("contact.html")
+
+    else:
+        return render_template("contact.html")
+
+@app.route("/mybeans")
+@login_required
+def mybeans():
+    return render_template("mybeans.html")
+
+@app.route("/reviews")
+@login_required
+def reviews():
+    return render_template("reviews.html")
---
+++ mailconfig.py
@@ -0,0 +1,2 @@
+mail_username = 'pierre_bruyninckx@hotmail.com'
+mail_password = 'B82rez2g4@'
